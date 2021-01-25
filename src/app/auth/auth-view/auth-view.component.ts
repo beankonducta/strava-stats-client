6db6033bc90b887d7b4a12e07eb41b82a6e3574e
patrick <patrickxandrews@gmail.com>
2021-01-25 10:07:14 -0700

    design tweaks

--- src/app/auth/auth-view/auth-view.component.ts
+++ src/app/auth/auth-view/auth-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-auth-view',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this.authService.login();
   }
 
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from "@angular/common/http";
+
 @Injectable({
   providedIn: 'root'
 })
@@ -7,5 +9,18 @@ export class AuthService {
 
   isLoggedIn: boolean = true;
 
-  constructor() { }
+  apiUrl: string = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  login() {
+    return this.http.get<any>(`${this.apiUrl}/`).subscribe(val => {
+      // this won't work until we get https on localhost
+      console.log(val);
+    })
+  }
+
+  logout() {
+
+  }
 }
