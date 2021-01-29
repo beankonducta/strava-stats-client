28f64d54fc7eb3e3deb6841059ac2f65dabf2561
patrick <patrickxandrews@gmail.com>
2021-01-29 09:39:14 -0700

    add cookie delete and router nav to error on stats service

--- src/app/stats/stats.service.ts
+++ src/app/stats/stats.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { take } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
@@ -13,19 +14,22 @@ export class StatsService {
   times: any;
   name: any;
 
-  constructor(private http: HttpClient, private cookieService: CookieService, private authService: AuthService) {
+  constructor(private http: HttpClient, private cookieService: CookieService, private authService: AuthService, private router: Router) {
     this.fetchData();
   }
 
   fetchData() {
     if(!this.cookieService.check('strava_auth')) { 
-      this.authService.fetchToken(this.cookieService.get('refresh_token'), true);
+      this.authService.fetchToken(this.cookieService.get('strava_refresh'), true);
     }
     const access = this.cookieService.get('strava_auth');
     this.http.get(`http://localhost:3000/athlete?token=${access}`).pipe(take(1)).subscribe((athlete: any) => {
       this.name = `${athlete.firstname} ${athlete.lastname}`;
       this.times = this.http.get(`http://localhost:3000/athletes/${athlete.id}/avg_mile_time?token=${access}`);
       this.stats = this.http.get(`http://localhost:3000/athletes/${athlete.id}/stats?token=${access}`);
+    }, error => {
+      this.cookieService.delete('strava_auth');
+      this.router.navigate(['/auth'])
     })
   }
 }
